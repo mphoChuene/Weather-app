be595faf130f3e763f98378c6e0182d62d3de385
mphoChuene <mphochuene42@gmail.com>
2023-07-20 10:08:12 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
+
+function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=88138dfdc16f7b57f722547ba4425c86`;
+
+  return (
+    <>
+      <h2>Running</h2>
+    </>
+  );
+}
+
+export default App;
