3ffbc9ed04ea09078646fecdae6a5f855b33977c
mphoChuene <mphochuene42@gmail.com>
2023-07-22 14:00:28 +0200

    search funciton troubleshooting

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,38 @@ import axios from "axios";
 import React, { useState } from "react";
 
 function App() {
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=88138dfdc16f7b57f722547ba4425c86`;
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState(" ");
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=88138dfdc16f7b57f722547ba4425c86`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    }
+  };
 
   return (
     <div className="app">
+      <div className="search">
+        <input
+          value={location}
+          onChange={(event) => searchLocation(event.target.value)}
+          placeholder=" Enter Location..."
+          onKeyPress={searchLocation}
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
             <p>Polokwane</p>
           </div>
           <div className="temp">
-            <h1>12 degrees celsius</h1>
+            <h1>12 degrees</h1>
           </div>
           <div className="description">
             <p>clouds</p>
@@ -21,12 +42,17 @@ function App() {
         </div>
         <div className="bottom">
           <div className="feels">
-            <p>12 degrees celsius</p>
+            <p className="bold">15 degrees</p>
+            <p>feels like</p>
           </div>
           <div className="humidity">
-            <p>20%</p>
+            <p className="bold">20%</p>
+            <p>Humidity</p>
           </div>
-          <div className="wind">12 MPH</div>
+          <div className="wind">
+            <p className="bold">12 MPH</p>
+            <p>Wind speed</p>
+          </div>
         </div>
       </div>
     </div>
